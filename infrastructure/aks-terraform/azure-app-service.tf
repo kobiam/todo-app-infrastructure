8f39288904a6977df1fc02126f35db205715c15c
kobiam <kobi700@gmail.com>
2025-06-22 18:44:49 +0300

    update identity to list, site_config, and app_settings

--- infrastructure/aks-terraform/azure-app-service.tf
+++ infrastructure/aks-terraform/azure-app-service.tf
@@ -19,16 +19,19 @@ resource "azurerm_service_plan" "main" {
   sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "main" {
+resource "azurerm_linux_web_app" "main" {
   name                = "fastapi-webapp-demo"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   app_service_plan_id = azurerm_service_plan.main.id
 
   site_config {
-    linux_fx_version = "DOCKER|todoappreg.azurecr.io/fastapi-backend:latest"
+    application_stack {
+      docker_image_name        = "backend:latest"
+      docker_registry_url      = "https://todoappreg.azurecr.io"
+    }
 
-    acr_use_managed_identity_credentials = true
+    always_on = true
   }
 
   identity {
@@ -37,14 +40,15 @@ resource "azurerm_app_service" "main" {
 
   app_settings = {
     WEBSITES_PORT = "8000"  # FastAPI default port
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
   }
 }
 
 # Grant App Service access to pull from ACR
 resource "azurerm_role_assignment" "acr_pull" {
-  scope                = data.azurerm_container_registry.acr.id
+  principal_id         = azurerm_linux_web_app.main.identity[0].principal_id
   role_definition_name = "AcrPull"
-  principal_id         = azurerm_app_service.main.identity.principal_id
+  scope                = azurerm_container_registry.main.id
 }
 
 data "azurerm_container_registry" "acr" {
